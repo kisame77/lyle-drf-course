47c49a08c2f303b55b51188a6968865d4c32a8f0
aurexus <aurexus@localhost.localdomain>
2024-12-09 17:58:09 +0530

    video 3 end

--- api/views.py
+++ api/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from api.models import Product
-from api.serializers import ProductSerializer
-
+from api.models import Order, Product
+from api.serializers import ProductSerializer,OrderSerializer
 
 # def product_list(request):
 #     products = Product.objects.all()
@@ -23,4 +22,11 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
