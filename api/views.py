f8a15dce983e1c897232d1d2e85daf021c30ecae
aurexus <aurexus@localhost.localdomain>
2024-12-10 12:45:46 +0530

    video 9 end

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
+from rest_framework.views import APIView
 from api.models import Order, Product
 from api.serializers import OrderSerializer, ProductInfoSerializer, ProductSerializer
 
@@ -39,6 +39,9 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     lookup_url_kwarg = "product_id"
 
 
+
+
+
 # @api_view(["GET"])
 # def order_list(request):
 #     orders = Order.objects.prefetch_related("items__product")
@@ -62,14 +65,27 @@ class UserOrderListAPIView(generics.ListAPIView):
         return qs.filter(user=user)
 
 
-@api_view(["GET"])
-def product_info(request):
-    products = Product.objects.all()
-    serializer = ProductInfoSerializer(
-        {
-            "products": products,
-            "count": len(products),
-            "max_price": products.aggregate(max_price=Max("price"))["max_price"],
-        }
-    )
-    return Response(serializer.data)
+class ProductInfoAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductInfoSerializer(
+            {
+                "products": products,
+                "count": len(products),
+                "max_price": products.aggregate(max_price=Max("price"))["max_price"],
+            }
+        )
+        return Response(serializer.data)
+
+
+# @api_view(["GET"])
+# def product_info(request):
+#     products = Product.objects.all()
+#     serializer = ProductInfoSerializer(
+#         {
+#             "products": products,
+#             "count": len(products),
+#             "max_price": products.aggregate(max_price=Max("price"))["max_price"],
+#         }
+#     )
+#     return Response(serializer.data)
