0323f98d602ca374eeb67aee84fe952bbf43c7da
aurexus <aurexus@localhost.localdomain>
2024-12-18 11:05:34 +0530

    video 21 complete

--- api/views.py
+++ api/views.py
@@ -2,13 +2,13 @@ from django.db.models import Max
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics, viewsets
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action, api_view
 from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.filters import InStockFilterBackend, ProductFilter
+from api.filters import InStockFilterBackend, ProductFilter, OrderFilter
 from api.models import Order, Product
 from api.serializers import OrderSerializer, ProductInfoSerializer, ProductSerializer
 
@@ -92,6 +92,19 @@ class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
     permission_classes = [AllowAny]
     pagination_class = None
+    filterset_class = OrderFilter
+    filter_backends = [DjangoFilterBackend]
+
+    @action(
+        detail=False,
+        methods=["get"],
+        url_path="user-orders",
+        permission_classes=[IsAuthenticated],
+    )
+    def user_orders(self, request):
+        orders = self.get_queryset().filter(user=request.user)
+        serializer = self.get_serializer(orders, many=True)
+        return Response(serializer.data)
 
 
 # class OrderListAPIView(generics.ListAPIView):
